d6e6dbff918480bac2e8848d13771c1b205e3658
Connor Shaw <connor@shawsoft.io>
2025-01-07 17:41:18 +0000

    remove dotenv dependacie in webjob

--- src/webjob/webjob.ts
+++ src/webjob/webjob.ts
@@ -1,7 +1,4 @@
 import { MongoClient } from "mongodb";
-import dotenv from "dotenv"
-
-dotenv.config()
 
 // MongoDB connection string from environment variables
 const uri = process.env.MONGODB_URI;
